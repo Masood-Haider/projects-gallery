a80492628ecc43a9c79a114b2b39540dde43646f
Masood-Haider <masood.haider.bangash1@gmail.com>
2026-07-24 15:57:25 +0500

    Add top margin/padding below fixed navbar on project description detail page

--- app/projects/[id]/ProjectDetailClient.tsx
+++ app/projects/[id]/ProjectDetailClient.tsx
@@ -37,7 +37,7 @@ export default function ProjectDetailClient({ project }: { project: Project }) {
     project.category === "frontend" ? "Frontend" : "Full Stack";
 
   return (
-    <div className="mx-auto max-w-6xl px-6 py-16">
+    <div className="mx-auto max-w-6xl px-4 sm:px-6 pb-20 pt-28 sm:pt-32">
       {/* Back link */}
       <Link
         href="/projects"
